1aa722b14295cd0d292032b2453a10bcd725df2f
G-Njunge <g.njunge@alustudent.com>
2025-08-21 20:20:30 +0200

    final edit

--- top_student.sql
+++ top_student.sql
@@ -5,10 +5,8 @@ SELECT
     lg.grade_obtained AS linux_grade, pg.grade_obtained AS python_grade, /*linux and python grades with aliases*/
     ROUND((lg.grade_obtained + pg.grade_obtained)/2, 2) AS average_grade /*calculating the average grade and rounding to two decimal places */
 FROM students s
--- join ensures that only students with both linux and python grades are included
-JOIN linux_grades lg ON s.student_id = lg.student_id /*adding of linux grades to each student based on their student id*/
+-- join ensures that only students with both linux and python grades are included 
+JOIN linux_grades lg ON s.student_id = lg.student_id  /*adding of linux grades to each student based on their student id*/
 JOIN python_grades pg ON s.student_id = pg.student_id /*adding of python grades to each student based on their student id*/
-
 ORDER BY average_grade DESC  /*Arranges students from highest to lowest*/
-
 LIMIT 1; -- Limits the list to only the top student
