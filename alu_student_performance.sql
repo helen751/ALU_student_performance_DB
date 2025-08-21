970ce087dfff3e65482fdf1f0b1a75c4eee25faf
TetaCindy <c.teta@alustudent.com>
2025-08-21 18:29:00 +0200

    Find student who took one course and those who took both

--- alu_student_performance.sql
+++ alu_student_performance.sql
@@ -74,3 +74,21 @@ INSERT INTO python_grades (student_id, grade_obtained) VALUES
 select * from students;
 select * from linux_grades;
 select * from python_grades;
+#Query 3: Find students who took only one course (either Linux or Python, not both).
+SELECT s.student_id, s.student_name, 'Linux Only' AS course_status
+FROM students s
+JOIN linux_grades l ON s.student_id = l.student_id
+WHERE s.student_id NOT IN (SELECT student_id FROM python_grades)
+
+UNION
+
+SELECT s.student_id, s.student_name, 'Python Only' AS course_status
+FROM students s
+JOIN python_grades p ON s.student_id = p.student_id
+WHERE s.student_id NOT IN (SELECT student_id FROM linux_grades);
+
+#Query 4: Find students who took both courses (Linux and Python).
+SELECT DISTINCT s.student_id, s.student_name
+FROM students s
+JOIN linux_grades l ON s.student_id = l.student_id
+JOIN python_grades p ON s.student_id = p.student_id;
