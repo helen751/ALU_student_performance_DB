d816e9adc7f00724e77781ecddaac8fc6f7060ce
shyakafrancis123 <s.francis@alustudent.com>
2025-08-21 17:57:09 +0200

    populated details

---
+++ populate.sql
@@ -0,0 +1,35 @@
+INSERT INTO students (student_name, intake_year) VALUES
+('Helen Okereke', 2025),
+('Grace Karimi Njunge', 2025),
+('Cindy Saro Teta', 2025),
+('Erioluwa Mercy Akintayo', 2025),
+('Bendou Janna Vitalina', 2025),
+('Shyaka Francis', 2025),
+('Kevin Muriithi', 2025),
+('Martha Nansubuga', 2025),
+('Tawanda Chikore', 2025),
+('Aisha Abdalla', 2025),
+('Jean Bosco Habimana', 2025),
+('Sarah Kwizera', 2025),
+('Daniel Okoth', 2025),
+('Fatoumata Diallo', 2025),
+('Peter Mugisha', 2025);
+
+
+INSERT INTO linux_grades (student_id, grade_obtained) VALUES
+(1, 85),
+(2, 90),
+(3, 78),
+(4, 92),
+(5, 80),
+(6, 88),
+(7, 75),
+(8, 83),
+(9, 70),
+(10, 95),
+(11, 76),
+(12, 89),
+(13, 81),
+(14, 67),
+(15, 93);
+
